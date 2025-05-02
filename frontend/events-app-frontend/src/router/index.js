d2548b728847593b9087c5445f675eb6b2184f29
Rafael Roque <roque.ata@hotmail.com>
2025-05-01 21:22:22 -0300

    fix frontend, cleaning backend

---
+++ frontend/events-app-frontend/src/router/index.js
@@ -0,0 +1,17 @@
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router'
+import EventList from '../components/EventList.vue' // Importe sua view
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'EventList',
+            component: EventList
+        },
+        // Você pode adicionar mais rotas aqui se necessário
+    ]
+})
+
+export default router
